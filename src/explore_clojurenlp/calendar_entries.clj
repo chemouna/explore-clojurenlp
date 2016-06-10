a758c83f081e7c5d9b48973ffa00e64b4da4c5fe
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-06-10 08:01:53 +0200

    add some exples.

---
+++ src/explore_clojurenlp/calendar_entries.clj
@@ -0,0 +1,4 @@
+(ns explore-clojurenlp.calendar-entries)
+
+;; goal: parse something like "Please schedule a meeting with Adam Smith and Sally Keynes, on November 22 2015, at 1:30pm, for 1 hour, to discuss the perils of economic forecasting."
+
