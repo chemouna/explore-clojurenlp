70a5881e2dd010d91e5b8a26e1cc8f2d3f2ab13b
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-06-10 05:30:34 +0200

    Init and clojure opennlp dep.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to explore-clojurenlp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/explore_clojurenlp/core_test.clj
@@ -0,0 +1,7 @@
+(ns explore-clojurenlp.core-test
+  (:require [clojure.test :refer :all]
+            [explore-clojurenlp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
